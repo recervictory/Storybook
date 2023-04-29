cd1ac80e28b16a54a8e86263ec25c96b1b1265b7
Victor Banerjee <recervictory@gmail.com>
2023-04-29 12:14:31 +0000

    Save mui complete button

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,22 +1,32 @@
+import type { Preview } from "@storybook/react";
+//* Mui 
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
+import { lightTheme, darkTheme } from '../src/theme';
 
-import '../src/index.css';
 
- // Registers the msw addon
- import { initialize, mswDecorator } from 'msw-storybook-addon';
 
- // Initialize MSW
- initialize();
-
- // Provide the MSW addon decorator globally
- export const decorators = [mswDecorator];
-
-//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+const preview: Preview = {
+  decorators : [
+    withThemeFromJSXProvider({
+    themes: {
+      light: lightTheme,
+      dark: darkTheme,
+    },
+    defaultTheme: 'light',
+    Provider: ThemeProvider,
+    GlobalStyles: CssBaseline,
+  })],
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        expanded: true, // Adds the description and default columns
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
     },
   },
 };
+
+export default preview;
